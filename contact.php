b1796bd744c00a76a7cc04cb80c824ae8c56aefe
doikenji <doi_k@extlink.co.jp>
2019-07-09 10:07:36 +0900

    second

--- contact.php
+++ contact.php
@@ -54,7 +54,7 @@ $(function() {
 });
 
 </script>
-<p>FTPであがれあがれあがれ</p>
+<p>FTPであがれあがれあがれあがれー！</p>
 <form id="contactForm" method="POST" enctype="multipart/form-data">
 
 <table class="tb tb2 contact_tb">
